e8262c742aac349cfb7fc61c4a8d3320b210a876
Brian Lach <brianlach72@gmail.com>
2022-02-13 00:52:47 -0500

    don't scale skybox by ambient light

--- src/shaders/skybox.frag.glsl
+++ src/shaders/skybox.frag.glsl
@@ -6,9 +6,9 @@
 in vec3 l_worldEyeToVert;
 uniform samplerCube skyboxSampler;
 
-uniform struct {
-    vec4 ambient;
-} p3d_LightModel;
+//uniform struct {
+//    vec4 ambient;
+//} p3d_LightModel;
 
 in vec4 l_color;
 
@@ -17,6 +17,6 @@ out vec4 outputColor;
 void main()
 {
     outputColor = texture(skyboxSampler, normalize(l_worldEyeToVert)) * l_color;
-    outputColor.rgb *= length(p3d_LightModel.ambient.rgb);
+    //outputColor.rgb *= length(p3d_LightModel.ambient.rgb);
 	FinalOutput(outputColor);
 }
