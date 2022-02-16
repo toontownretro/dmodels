f2abeda9cb67540003a685328a36e241c1e9025b
Brian Lach <brianlach72@gmail.com>
2022-02-16 13:18:16 -0500

    improve bloom shader

--- src/shaders/postprocess/bloom_downsample.frag.glsl
+++ src/shaders/postprocess/bloom_downsample.frag.glsl
@@ -6,16 +6,11 @@ uniform writeonly image2D destTexture;
 uniform ivec2 mipVec;
 #define mip (mipVec.x)
 
-uniform vec2 streakLengthVec;
-#define streakLength (streakLengthVec.x)
-
 void main() {
   ivec2 intCoord = ivec2(gl_FragCoord.xy);
 
   vec2 parentTexSize = vec2(textureSize(sourceTexture, mip).xy);
   vec2 texelSize = 1.0 / parentTexSize;
-
-  vec2 offset = vec2(streakLength * texelSize.x, texelSize.y);
 
   // Compute the floating point coordinate pointing to the exact center of the
   // parent texel center.
@@ -26,32 +21,38 @@ void main() {
   vec3 centerSample = textureLod(sourceTexture, fltCoord, mip).rgb;
 
   // inner samples (marked red)
-  vec3 sample_r_tl = textureLod(sourceTexture, fltCoord + vec2(-1, 1) * offset, mip).rgb;
-  vec3 sample_r_tr = textureLod(sourceTexture, fltCoord + vec2(1, 1) * offset, mip).rgb;
-  vec3 sample_r_bl = textureLod(sourceTexture, fltCoord + vec2(-1, -1) * offset, mip).rgb;
-  vec3 sample_r_br = textureLod(sourceTexture, fltCoord + vec2(1, -1) * offset, mip).rgb;
+  vec3 sample_r_tl = textureLod(sourceTexture, fltCoord + vec2(-1, 1) * texelSize, mip).rgb;
+  vec3 sample_r_tr = textureLod(sourceTexture, fltCoord + vec2(1, 1) * texelSize, mip).rgb;
+  vec3 sample_r_bl = textureLod(sourceTexture, fltCoord + vec2(-1, -1) * texelSize, mip).rgb;
+  vec3 sample_r_br = textureLod(sourceTexture, fltCoord + vec2(1, -1) * texelSize, mip).rgb;
 
   // corner samples
-  vec3 sample_t = textureLod(sourceTexture, fltCoord + vec2(0, 2) * offset, mip).rgb;
-  vec3 sample_r = textureLod(sourceTexture, fltCoord + vec2(2, 0) * offset, mip).rgb;
-  vec3 sample_b = textureLod(sourceTexture, fltCoord + vec2(0, -2) * offset, mip).rgb;
-  vec3 sample_l = textureLod(sourceTexture, fltCoord + vec2(-2, 0) * offset, mip).rgb;
+  vec3 sample_t = textureLod(sourceTexture, fltCoord + vec2(0, 2) * texelSize, mip).rgb;
+  vec3 sample_r = textureLod(sourceTexture, fltCoord + vec2(2, 0) * texelSize, mip).rgb;
+  vec3 sample_b = textureLod(sourceTexture, fltCoord + vec2(0, -2) * texelSize, mip).rgb;
+  vec3 sample_l = textureLod(sourceTexture, fltCoord + vec2(-2, 0) * texelSize, mip).rgb;
 
   // edge samples
-  vec3 sample_tl = textureLod(sourceTexture, fltCoord + vec2(-2, 2) * offset, mip).rgb;
-  vec3 sample_tr = textureLod(sourceTexture, fltCoord + vec2(2, 2) * offset, mip).rgb;
-  vec3 sample_bl = textureLod(sourceTexture, fltCoord + vec2(-2, -2) * offset, mip).rgb;
-  vec3 sample_br = textureLod(sourceTexture, fltCoord + vec2(2, -2) * offset, mip).rgb;
+  vec3 sample_tl = textureLod(sourceTexture, fltCoord + vec2(-2, 2) * texelSize, mip).rgb;
+  vec3 sample_tr = textureLod(sourceTexture, fltCoord + vec2(2, 2) * texelSize, mip).rgb;
+  vec3 sample_bl = textureLod(sourceTexture, fltCoord + vec2(-2, -2) * texelSize, mip).rgb;
+  vec3 sample_br = textureLod(sourceTexture, fltCoord + vec2(2, -2) * texelSize, mip).rgb;
 
-  vec3 kernelSumRed = (sample_r_tl + sample_r_tr + sample_r_bl + sample_r_br) * 0.25;
-  vec3 kernelSumYellow = (sample_tl + sample_t + sample_l + centerSample) * 0.25;
-  vec3 kernelSumGreen = (sample_tr + sample_t + sample_r + centerSample) * 0.25;
-  vec3 kernelSumPurple = (sample_bl + sample_b + sample_l + centerSample) * 0.25;
-  vec3 kernelSumBlue = (sample_br + sample_b + sample_r + centerSample) * 0.25;
+  vec3 kernelSumRed = (sample_r_tl + sample_r_tr + sample_r_bl + sample_r_br);
+  vec3 kernelSumYellow = (sample_tl + sample_t + sample_l + centerSample);
+  vec3 kernelSumGreen = (sample_tr + sample_t + sample_r + centerSample);
+  vec3 kernelSumPurple = (sample_bl + sample_b + sample_l + centerSample);
+  vec3 kernelSumBlue = (sample_br + sample_b + sample_r + centerSample);
 
   vec3 summedKernel = kernelSumRed * 0.5 + kernelSumYellow * 0.125 +
                       kernelSumGreen * 0.125 + kernelSumPurple * 0.125 +
                       kernelSumBlue * 0.125;
+
+  // Since every sub-kernel has 4 samples, normalize that.
+  summedKernel *= 0.25;
+
+  // Decay.
+  summedKernel *= 1.3;
 
   imageStore(destTexture, intCoord, vec4(summedKernel, 0.0));
 }
--- src/shaders/postprocess/extract_bright_spots.frag.glsl
+++ src/shaders/postprocess/extract_bright_spots.frag.glsl
@@ -5,9 +5,8 @@ in vec2 l_texcoord;
 uniform sampler2D sourceTexture;
 uniform writeonly image2D destTexture;
 
-uniform vec4 curve_threshold;
-#define curve (curve_threshold.xyz)
-#define threshold (curve_threshold.w)
+uniform vec2 bloomStrengthVec;
+#define bloomStrength (bloomStrengthVec.x)
 
 const vec3 luminance_weights = vec3(0.2125, 0.7154, 0.0721);
 
@@ -16,15 +15,11 @@ void main() {
 
   vec3 sceneColor = textureLod(sourceTexture, l_texcoord, 0).rgb;
 
-  float br = dot(sceneColor, luminance_weights);
+  float luma = dot(sceneColor, luminance_weights);
 
-  // Under-threshold: quadratic curve
-  float rq = clamp(br - curve.x, 0, curve.y);
-  rq = curve.z * rq * rq;
-
-  // Combine and apply the brightness response curve.
   vec3 bloomColor = sceneColor;
-  bloomColor *= max(rq, br - threshold) / max(br, 1e-5);
+  bloomColor *= bloomStrength * 0.005;
+  bloomColor = clamp(bloomColor, vec3(0), vec3(25000));
 
   imageStore(destTexture, coord, vec4(bloomColor, 0.0));
 }
