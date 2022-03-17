09ceab8f9236d6ef0dd1e79f842c6ae87b893556
Brian Lach <brianlach72@gmail.com>
2022-03-17 17:50:54 -0400

    port depth

---
+++ src/shadersnew/depth.frag.glsl
@@ -0,0 +1,62 @@
+// Depth-only shader for z-prepass or shadow maps.
+// Does alpha cutouts.
+
+#version 330
+
+#pragma combo BASETEXTURE 0 1
+#pragma combo HAS_ALPHA 0 1
+#pragma combo CLIPPING 0 1
+
+// We only care about the basetexture if alpha is enabled.
+#pragma skip $[and $[not $[HAS_ALPHA]],$[BASETEXTURE]]
+
+/**
+ * @file depth.frag.glsl
+ * @author lachbr
+ * @date 2020-12-16
+ */
+
+#extension GL_GOOGLE_include_directive : enable
+#include "shadersnew/common_frag.inc.glsl"
+
+in vec3 l_texcoord_alpha;
+in vec4 l_worldPosition;
+
+// Clip planes.
+#if CLIPPING
+uniform vec4 p3d_WorldClipPlane[4];
+layout(constant_id = 0) const int NUM_CLIP_PLANES = 0;
+#endif
+
+#if BASETEXTURE
+uniform sampler2D baseTextureSampler;
+#endif
+
+#if HAS_ALPHA
+layout(constant_id = 1) const int ALPHA_TEST_MODE = 7;
+layout(constant_id = 2) const float ALPHA_TEST_REF = 0.5;
+#endif
+
+void main() {
+#if CLIPPING
+  int clip_plane_count = min(4, NUM_CLIP_PLANES);
+  for (int i = 0; i < clip_plane_count; ++i) {
+    if (dot(p3d_WorldClipPlane[i], l_worldPosition) < 0.0) {
+      discard;
+    }
+  }
+#endif
+
+#if HAS_ALPHA
+#if BASETEXTURE
+  float alpha = texture(baseTextureSampler, l_texcoord_alpha.xy).a;
+  alpha *= l_texcoord_alpha.z;
+#else
+  float alpha = l_texcoord_alpha.z;
+#endif
+
+  if (!do_alpha_test(alpha, ALPHA_TEST_MODE, ALPHA_TEST_REF)) {
+    discard;
+  }
+#endif // HAS_ALPHA
+}
---
+++ src/shadersnew/depth.vert.glsl
@@ -0,0 +1,62 @@
+// Depth-only shader for z-prepass or shadow maps.
+// Does alpha cutouts.
+
+#version 330
+
+#pragma combo BASETEXTURE 0 1
+#pragma combo SKINNING 0 1
+
+/**
+ * @file depth.vert.glsl
+ * @author lachbr
+ * @date 2020-12-16
+ */
+
+#extension GL_GOOGLE_include_directive : enable
+
+#include "shadersnew/common_vert.inc.glsl"
+
+uniform mat4 p3d_ViewMatrix;
+uniform mat4 p3d_ProjectionMatrix;
+uniform mat4 p3d_ModelMatrix;
+uniform vec4 p3d_ColorScale;
+
+in vec4 p3d_Vertex;
+in vec4 p3d_Color;
+in vec2 texcoord;
+
+out vec3 l_texcoord_alpha;
+out vec4 l_worldPosition;
+
+#if !BASETEXTURE
+uniform vec4 baseColor;
+#endif
+
+#if SKINNING
+uniform mat4 p3d_TransformTable[120];
+in vec4 transform_weight;
+in uvec4 transform_index;
+#endif
+
+void main() {
+  vec4 finalVertex = p3d_Vertex;
+
+#if SKINNING
+  vec3 foo = vec3(0);
+  do_skinning(p3d_Vertex, vec3(0), p3d_TransformTable, transform_weight, transform_index,
+              false, finalVertex, foo);
+#endif
+
+  vec4 worldPos = p3d_ModelMatrix * finalVertex;
+  vec4 eyePos = p3d_ViewMatrix * worldPos;
+  gl_Position = p3d_ProjectionMatrix * eyePos;
+
+  float alpha = p3d_ColorScale.a * p3d_Color.a
+    #if !BASETEXTURE
+    * baseColor.a
+    #endif
+    ;
+  l_texcoord_alpha = vec3(texcoord, alpha);
+
+  l_worldPosition = worldPos;
+}
