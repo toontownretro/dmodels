e7182a8636609d8f186e091e9cbfe510e28f4501
Brian Lach <brianlach72@gmail.com>
2022-12-08 17:48:48 -0500

    remove old, unused shaders
    most of these were replaced with shadersnew versions

--- src/shaders/Sources.pp
+++ src/shaders/Sources.pp
@@ -3,29 +3,12 @@
 
 #begin install_shader
   #define SOURCES \
-    common_animation_vert.inc.glsl \
-    common_brdf_frag.inc.glsl \
-    common_fog_frag.inc.glsl \
-    common_frag.inc.glsl \
-    common_lighting_frag.inc.glsl \
     common_sequences.inc.glsl \
-    common_shadows_frag.inc.glsl \
-    common_shadows_vert.inc.glsl \
-    common_vert.inc.glsl \
     common.inc.glsl \
     compress_bc6h.compute.glsl \
-    csmdepth.frag.glsl \
-    csmdepth.geom.glsl \
-    csmdepth.vert.glsl \
     cubemap_filter.compute.glsl \
     debug_csm.frag.glsl \
     debug_csm.vert.glsl \
-    depth.frag.glsl \
-    depth.vert.glsl \
-    eyes.frag.glsl \
-    eyes.vert.glsl \
-    lightmappedGeneric_PBR.frag.glsl \
-    lightmappedGeneric_PBR.vert.glsl \
     lm_buffers.inc.glsl lm_compute.inc.glsl \
     lm_direct.compute.glsl lm_indirect.compute.glsl lm_unocclude.compute.glsl \
     lm_raster.frag.glsl lm_raster.vert.glsl \
@@ -33,14 +16,6 @@
     lm_dilate.compute.glsl \
     lm_vtx_direct.compute.glsl \
     lm_vtx_indirect.compute.glsl \
-    lm_vtx_raster.vert.glsl lm_vtx_raster.frag.glsl \
-    skybox.frag.glsl \
-    skybox.vert.glsl \
-    source_vlg.vert.glsl source_vlg.frag.glsl source_vlg_orig.frag.glsl \
-    spriteParticle.vert.glsl spriteParticle.geom.glsl spriteParticle.frag.glsl \
-    unlitNoMat.frag.glsl \
-    unlitNoMat.vert.glsl \
-    vertexLitGeneric_PBR.frag.glsl \
-    vertexLitGeneric_PBR.vert.glsl \
-    source_sky.vert.glsl source_sky.frag.glsl
+    lm_vtx_raster.vert.glsl lm_vtx_raster.frag.glsl
+
 #end install_shader
