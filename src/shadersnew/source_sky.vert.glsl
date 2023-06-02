a8fc28e87ae32ce8e9b3d234cc83510d8d6e46ba
Brian Lach <brianlach72@gmail.com>
2023-06-01 22:53:51 -0400

    source_sky: get rid of weird algorithm
    we just use a premade skybox model now

--- src/shadersnew/source_sky.vert.glsl
+++ src/shadersnew/source_sky.vert.glsl
@@ -9,18 +9,14 @@
 
 #pragma combo COMPRESSED_HDR 0 1
 
-uniform mat4 p3d_ViewProjectionMatrix;
-uniform mat4 p3d_ModelMatrix;
+uniform mat4 p3d_ModelViewProjectionMatrix;
 
 in vec4 p3d_Vertex;
+in vec2 texcoord;
 
 uniform vec3 wspos_view;
 
 uniform mat4 skyTexTransform;
-
-uniform vec2 u_zFar_index;
-#define zFar (u_zFar_index.x)
-#define index (u_zFar_index.y)
 
 #if COMPRESSED_HDR
 
@@ -42,75 +38,30 @@ out vec2 l_texcoord;
 
 #endif
 
-#define SQRT3INV 0.57735
-
-const ivec3 st_to_vec[6] = ivec3[6](
-  ivec3(3,-1,2),
-  ivec3(-3,1,2),
-
-  ivec3(1,3,2),
-  ivec3(-1,-3,2),
-
-  ivec3(-2,-1,3),
-  ivec3(2,-1,-3)
-);
-
-void
-make_sky_vec(float s, float t, int axis, out vec3 position, out vec2 texcoord) {
-  float width = zFar * SQRT3INV;
-
-  s = clamp(s, -1.0, 1.0);
-  t = clamp(t, -1.0, 1.0);
-
-  vec3 b = vec3(s * width, t * width, width);
-  vec3 v = vec3(0);
-
-  for (int j = 0; j < 3; ++j) {
-    int k = st_to_vec[axis][j];
-    if (k < 0) {
-      v[j] = -b[-k - 1];
-    } else {
-      v[j] = b[k - 1];
-    }
-    v[j] += wspos_view[j];
-  }
-
-  // Avoid bilerp seam.
-  s = (s + 1) * 0.5;
-  t = (t + 1) * 0.5;
-
-  s = clamp(s, 1.0/512.0, 511.0/512.0);
-  t = clamp(t, 1.0/512.0, 511.0/512.0);
-
-  //t = 1.0 - t;
-  position = v;
-  texcoord = vec2(s, t);
-}
-
 void
 main() {
-  vec3 skyPos = vec3(0);
-  vec2 texcoord = vec2(0);
-  make_sky_vec(p3d_Vertex.x, p3d_Vertex.z, int(index), skyPos, texcoord);
+  //vec3 skyPos = vec3(0);
+  //vec2 texcoord = vec2(0);
+  //make_sky_vec(p3d_Vertex.x, p3d_Vertex.z, int(index), skyPos, texcoord);
 
-  gl_Position = p3d_ViewProjectionMatrix * vec4(skyPos, 1);
+  gl_Position = p3d_ModelViewProjectionMatrix * p3d_Vertex;
 
-  texcoord = (skyTexTransform * vec4(texcoord, 1, 1)).xy;
+  vec2 vtexcoord = (skyTexTransform * vec4(texcoord, 1, 1)).xy;
 
 #if COMPRESSED_HDR
-  l_texcoord00.x = texcoord.x - TEXEL_XINCR;
-  l_texcoord00.y = texcoord.y - TEXEL_YINCR;
-  l_texcoord10.x = texcoord.x + TEXEL_XINCR;
-  l_texcoord10.y = texcoord.y - TEXEL_YINCR;
-  l_texcoord01.x = texcoord.x - TEXEL_XINCR;
-  l_texcoord01.y = texcoord.y + TEXEL_YINCR;
-  l_texcoord11.x = texcoord.x + TEXEL_XINCR;
-  l_texcoord11.y = texcoord.y + TEXEL_YINCR;
+  l_texcoord00.x = vtexcoord.x - TEXEL_XINCR;
+  l_texcoord00.y = vtexcoord.y - TEXEL_YINCR;
+  l_texcoord10.x = vtexcoord.x + TEXEL_XINCR;
+  l_texcoord10.y = vtexcoord.y - TEXEL_YINCR;
+  l_texcoord01.x = vtexcoord.x - TEXEL_XINCR;
+  l_texcoord01.y = vtexcoord.y + TEXEL_YINCR;
+  l_texcoord11.x = vtexcoord.x + TEXEL_XINCR;
+  l_texcoord11.y = vtexcoord.y + TEXEL_YINCR;
 
   l_texcoord_pixels.xy = l_texcoord00;
   l_texcoord_pixels.x *= U_TO_PIXEL_COORD_SCALE;
   l_texcoord_pixels.y *= V_TO_PIXEL_COORD_SCALE;
 #else
-  l_texcoord = texcoord;
+  l_texcoord = vtexcoord;
 #endif
 }
